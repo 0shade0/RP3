93f0d2aa7d68854db535c8dec1be4809996ce4a1
Tihana Štifanić <sttihan.math@pmf.hr>
2020-12-26 23:01:51 +0100

    Dodane klase Pacman i movableCharacter i bazično pomicanje po ekranu.

--- Pacman/Form1.cs
+++ Pacman/Form1.cs
@@ -12,9 +12,23 @@ namespace Pacman
 {
     public partial class Form1 : Form
     {
+        Pacman pacman;
         public Form1()
         {
             InitializeComponent();
+            pacman = new Pacman(this);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+                pacman.Dir = Pacman.Direction.Left;
+            else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                pacman.Dir = Pacman.Direction.Up;
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+                pacman.Dir = Pacman.Direction.Right;
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                pacman.Dir = Pacman.Direction.Down;
         }
     }
 }
---
+++ Pacman/movableCharacter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public class movableCharacter
+    {
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            None
+        }
+        
+        protected int x, y; // Koordinate u polju.
+        protected int speed = 10;
+        protected Direction dir = Direction.None;
+        protected PictureBox characterPictureBox = new PictureBox();
+        Timer characterTimer = new Timer();
+
+        // Konstruktor.
+        public movableCharacter(Form form)
+        {
+            // Postavljanje timera. Event handler characterTimerTick
+            // se izvodi svakih 100ms.
+            characterTimer.Interval = 100;
+            characterTimer.Enabled = true;
+            characterTimer.Tick += new EventHandler(characterTimerTick);
+            characterTimer.Start();
+            characterPictureBox.BackColor = Color.Yellow;
+
+            form.Controls.Add(characterPictureBox);
+        }
+
+        private void characterTimerTick (object sender, EventArgs e)
+        {
+            moveCharacter();
+            drawCharacter();
+        }
+
+        // Svojstva.
+        public Direction Dir
+        {
+            get { return dir; }
+            set { dir = value; }
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public void moveCharacter()
+        {
+            if (dir == Direction.Left)
+                x -= speed;
+            else if (dir == Direction.Right)
+                x += speed;
+            else if (dir == Direction.Up)
+                y -= speed;
+            else if (dir == Direction.Down)
+                y += speed;
+        }
+
+        public void drawCharacter()
+        {
+            characterPictureBox.Location = new Point(x, y);
+        }
+
+    }
+}
